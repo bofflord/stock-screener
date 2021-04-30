5dd41f470e1c0a61395e5942904133c5662c75d1
bofflord <boffinet@web.de>
2021-04-30 23:51:53 +0200

    overall refinement and further development

--- src/get_prices.py
+++ src/get_prices.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import yfinance as yf
 import os, contextlib
+import glob
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType 
+from pyspark.sql.types import ArrayType, DoubleType, DateType
+from pyspark.sql import functions as F
 
-def download_ticker_prices(symbol_list, period='max'):
+def download_ticker_prices(symbol_list, period='10y'):
     """download prices from yfinance for all symbols in a list of the defined period.
 
     Args:
@@ -25,3 +31,54 @@ def download_ticker_prices(symbol_list, period='max'):
                 data.to_csv('../data/3_prices/{}.csv'.format(s))
     print('Total number of valid symbols downloaded = {}'.format(sum(is_valid)))
     
+def load_ticker_prices(spark, symbol_list):
+    """[summary]
+
+    Args:
+        spark ([type]): [description]
+        symbol_list ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """    
+    file_list = glob.glob('../data/3_prices/*.csv')
+    file_list = [file_path for file_path in file_list\
+                    if any(symbol in file_path for symbol in symbol_list)]
+    schema = StructType() \
+                .add("Date",DateType(),True) \
+                .add("Ticker",StringType(),True) \
+                .add("Open",DoubleType(),True) \
+                .add("High",DoubleType(),True) \
+                .add("Low",DoubleType(),True) \
+                .add("Close",DoubleType(),True) \
+                .add("Adj Close",DoubleType(),True) \
+                .add("Volume",DoubleType(),True) 
+    df = spark.read.option('header',True)\
+                    .schema(schema)\
+                    .csv(file_list)
+    df.printSchema()
+    return df
+
+def calculate_annual_price(spark, price_df, time_period):
+    # create column month
+    price_df = price_df.withColumn('month', F.month(price_df['Date']))
+    # create column year
+    price_df = price_df.withColumn('year', F.year(price_df['Date']))
+    # filter for time period
+    price_df = price_df.filter(
+                    (price_df['year']>=time_period['start_date']) & \
+                    (price_df['year']<=time_period['end_date'])
+                                )
+    # filter for month December (==12)
+    price_df = price_df.filter(price_df['month']==12)
+    # group by Ticker and aggregate on median
+    ann_price_df = price_df.select('year', 'Ticker', 'Low')\
+                            .groupBy('year', 'Ticker')\
+                            .agg(F.mean('Low')\
+                                    .alias('mean_low_price'))\
+                            .orderBy('year', 'Ticker')
+    # convert Spark Dataframe to Pandas Dataframe
+    ann_price_df = ann_price_df.toPandas()
+    return ann_price_df
+
+
